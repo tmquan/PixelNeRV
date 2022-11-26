90a8782320738a6138051b81fa497a8d3bfe7dbc
tmquan <quantm88@gmail.com>
2022-11-26 04:22:56 +0000

    mixture net dropout 0.4

--- unet/inverse_renderer.py
+++ unet/inverse_renderer.py
@@ -20,14 +20,14 @@ class UnetFrontToBackInverseRenderer(nn.Module):
                 spatial_dims=2,
                 in_channels=self.in_channels,
                 out_channels=shape,
-                channels=encoder_feature_channel["efficientnet-l2"],
+                channels=encoder_feature_channel["efficientnet-b8"],
                 strides=(2, 2, 2, 2),
                 num_res_units=4,
                 kernel_size=3,
                 up_kernel_size=3,
                 act=("LeakyReLU", {"inplace": True}),
+                dropout=0.4,
                 norm=Norm.BATCH,
-                dropout=0.5,
             ),
             Reshape(*[1, shape, shape, shape]),
         )
@@ -36,22 +36,38 @@ class UnetFrontToBackInverseRenderer(nn.Module):
             Unet(
                 spatial_dims=3,
                 in_channels=1,
-                out_channels=self.out_channels+1,
-                channels=encoder_feature_channel["efficientnet-l2"],
+                out_channels=1,
+                channels=encoder_feature_channel["efficientnet-b8"],
                 strides=(2, 2, 2, 2),
                 num_res_units=2,
                 kernel_size=3,
                 up_kernel_size=3,
                 act=("LeakyReLU", {"inplace": True}),
+                dropout=0.4,
                 norm=Norm.BATCH,
-                dropout=0.5,
+            ),
+        )
+
+        self.mixture_net = nn.Sequential(
+            Unet(
+                spatial_dims=3,
+                in_channels=2,
+                out_channels=self.out_channels+1,
+                channels=encoder_feature_channel["efficientnet-b8"],
+                strides=(2, 2, 2, 2),
+                num_res_units=2,
+                kernel_size=3,
+                up_kernel_size=3,
+                act=("LeakyReLU", {"inplace": True}),
+                dropout=0.4,
+                norm=Norm.BATCH,
             ),
         )
         
     def forward(self, figures):
         clarity = self.clarity_net(figures)
-        volumes_opacits = self.density_net(clarity)
-
+        density = self.density_net(clarity)
+        volumes_opacits = self.mixture_net(torch.cat([clarity, density], dim=1))
         volumes,opacits = torch.split(volumes_opacits, [self.out_channels, 1], dim=1)
         return volumes, F.softplus(opacits)
         
