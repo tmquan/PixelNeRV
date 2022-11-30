e1ece8934ef9feb5da48440c96f3076298770d8a
tmquan <quantm88@gmail.com>
2022-11-30 23:30:07 +0000

    init to xavier, sigmoid rgb, softplut opacity

--- main_unetv.py
+++ main_unetv.py
@@ -26,6 +26,7 @@ from pytorch3d.renderer.cameras import (
     look_at_view_transform
 )
 
+
 from pytorch_lightning.utilities.seed import seed_everything
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 from pytorch_lightning.callbacks import LearningRateMonitor
@@ -39,7 +40,36 @@ from datamodule import UnpairedDataModule
 from unet.inverse_renderer import UnetFrontToBackInverseRenderer
 from dvr.renderer import DirectVolumeFrontToBackRenderer
 
-
+def init_weights(net, init_type='normal', init_gain=0.02):
+    """Initialize network weights.
+    Parameters:
+        net (network)   -- network to be initialized
+        init_type (str) -- the name of an initialization method: normal | xavier | kaiming | orthogonal
+        init_gain (float)    -- scaling factor for normal, xavier and orthogonal.
+    We use 'normal' in the original pix2pix and CycleGAN paper. But xavier and kaiming might
+    work better for some applications. Feel free to try yourself.
+    """
+    def init_func(m):  # define the initialization function
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            if init_type == 'normal':
+                nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                nn.init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                nn.init.constant_(m.bias.data, 0.0)
+        elif classname.find('BatchNorm') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
+            nn.init.normal_(m.weight.data, 1.0, init_gain)
+            nn.init.constant_(m.bias.data, 0.0)
+    # print('initialize network with %s' % init_type)
+    net.apply(init_func)  # apply the initialization function <init_func>
+    
 class UnetLightningModule(LightningModule):
     def __init__(self, hparams, **kwargs):
         super().__init__()
@@ -70,7 +100,6 @@ class UnetLightningModule(LightningModule):
             min_depth=2.0, 
             max_depth=6.0
         )
-        
         if self.n_harmonic_functions_xyz==0 and self.n_harmonic_functions_dir==0:
             in_channels = 1 + 3 + 3
         else:
@@ -84,6 +113,8 @@ class UnetLightningModule(LightningModule):
             out_channels=1, 
         )
 
+        init_weights(self.inv_renderer, init_type='xavier', init_gain=0.02)
+        
         self.loss_smoothl1 = nn.SmoothL1Loss(reduction="mean", beta=0.02)
          
     def forward(self, figures, bundle):
