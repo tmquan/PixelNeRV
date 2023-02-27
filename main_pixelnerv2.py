0c811b263820586babb9b5c68d27bef71c06d21e
tmquan <quantm88@gmail.com>
2023-02-27 04:57:38 +0000

    Remove abs shcomps

--- main_pixelnerv2.py
+++ main_pixelnerv2.py
@@ -198,9 +198,12 @@ class PixelNeRVFrontToBackInverseRenderer(nn.Module):
             # shcomps = shcoeff*self.shbasis.repeat(clarity.shape[0], 1, 1, 1, 1) 
             sh_comps_raw = torch.einsum('abcde,bcde->abcde', shcoeff, self.shbasis)
             # Take the absolute value of the spherical harmonic components
-            sh_comps_abs = torch.abs(sh_comps_raw)
+            # sh_comps_abs = torch.abs(sh_comps_raw)
+            sh_comps_max = sh_comps_raw.max()
+            sh_comps_min = sh_comps_raw.min()
             # Normalize the spherical harmonic components
-            shcomps = sh_comps_abs / (torch.max(sh_comps_abs) + 1e-8)
+            # shcomps = (sh_comps_abs - sh_comps_min) / (sh_comps_max - sh_comps_min + 1e-8)
+            shcomps = (sh_comps_raw - sh_comps_min) / (sh_comps_max - sh_comps_min + 1e-8)
         else:
             shcomps = shcoeff 
 
